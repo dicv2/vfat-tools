73ceb7eccc6bc0bd929d85e94d46d74cbc9c8590
Dimosgr <dimos_athens_24@yahoo.gr>
2023-03-02 11:13:46 +0000

    started velo_airdrop

--- src/static/js/optimism.js
+++ src/static/js/optimism.js
@@ -17,14 +17,15 @@ const main = async() => {
       ["Synthetix             ", `<a href="synthetix"         >Various</a>`,"SNX          ","https://staking.synthetix.io"],
       ["Thales                ", `<a href="thales"            >Various</a>`,"THALES       ","https://thalesmarket.io"],
       ["Stargate              ", `<a href="stargate"          >Various</a>`,"STG          ","https://stargate.finance"],
-      ["Hundred *             ", `<a href="hundred"           >Various</a>`,"HND          ","https://hundred.finance"],
+      ["Hundred               ", `<a href="hundred"           >Various</a>`,"HND          ","https://hundred.finance"],
       ["Xtoken Terminal       ", `<a href="xtokenterminal"    >Various</a>`,"HND          ","https://app.xtokenterminal.io"],
       ["Velodrome             ", `<a href="velodrome"         >Various</a>`,"VELO         ","https://app.velodrome.finance"],
       ["Lyra                  ", `<a href="lyra"              >Various</a>`,"LYRA         ","https://app.lyra.finance"],
       ["Degen Eth             ", `<a href="degeneth"          >Various</a>`,"BANDIT       ","https://degeneth.xyz/"],
       ["OpenXSwap             ", `<a href="openxswap"         >Various</a>`,"OPENX        ","https://openxswap.exchange"],
       ["Hop Exchange          ", `<a href="hop"               >Various</a>`,"HOP          ","https://app.hop.exchange"],
-      ["Reaper.Farm           ", `<a href="reaperfarm"        >Various</a>`,"Various      ","https://reaper.farm"]
+      ["Reaper.Farm           ", `<a href="reaperfarm"        >Various</a>`,"Various      ","https://reaper.farm"],
+      ["Velodrome Airdrop     ", `<a href="velo_airdrop"      >Various</a>`,"             ",""]
     ].reverse()
   }
 
